dc43006f07e0bb426ee8511054c8df64a5c15c95
Wiener234 <wiemernils@gmail.com>
2023-05-01 21:49:05 +0200

    Aud weekly 01 final

--- clara/koma.py
+++ clara/koma.py
@@ -1,4 +1,5 @@
 import math
+import timeit
 import itertools as it
 #import random 
 
@@ -7,11 +8,10 @@ import itertools as it
     #for a in range(int((n/2)**(1/3)), int(math.sqrt(n))+1)[::-1]:
     #for a in range(544949140000, int(math.sqrt(n))+1):
 def find_a_and_b(n):
-    list = it.combinations(range(int(math.sqrt(n)+1)), 1)
 #        print(a)
-    for a in list:
+    #for a in list:
+    for a in range(int((n/2)**(1/3)), int(math.sqrt(n))+1):
         if n % a == 0:
-            print(a)
             b1 = -a/2 + math.sqrt((a**2)/4 + n/a)
             b2 = -a/2 - math.sqrt((a**2)/4 + n/a)
             if b1 > 0 and (n % b1) == 0 and (n % (a+b1)) == 0:
@@ -21,7 +21,12 @@ def find_a_and_b(n):
     return None 
 
 
-print(find_a_and_b(102123161417560384731360000))
+#print(find_a_and_b(102123161417560384731360000))
+for b in range(2, 27):
+    start = timeit.default_timer()
+    print(b,": ",find_a_and_b(10**b))
+    stop = timeit.default_timer()
+    print("Time for 10^", b, ": ", stop-start)
 #_break = True
 #
 #while _break:
@@ -40,13 +45,16 @@ print(find_a_and_b(102123161417560384731360000))
 #n=102123161417560384731360000
 #finde a und b (a,b) Element der natürlichen Zahlen
 
-#def ab(n):
-#    for a in range(int((n/2)**(1/3)), int(n**(1/2))):
-#        for b in range(1, int((n/2*a)**(1/2))):
-#            if n == a*b*(a+b):
-#                return a,b
-#    return 0,0
-#
+def ab(n):
+    for a in range(int((n/2)**(1/3)), int(n**(1/2))):
+        for b in range(1, int((n/2*a)**(1/2))):
+            if n == a*b*(a+b):
+                return a,b
+    return 0,0
+
+#for b in range(2, 27):
+#print(": ",ab(10**17))
+
 #ab(102123161417560384731360000)
 #    
 
--- clara/test.py
+++ clara/test.py
@@ -1,6 +1,6 @@
 import itertools
-import math
 n = 102123161417560384731360000
-a = itertools.combinations(range(int(math.sqrt(n))+1), 1)
-for i in a:
-    print(i)
+a = itertools.combinations(range(int((n/2)**(1/3)),int(math.sqrt(n)+1)), 1)
+
+#for i in a:
+#    print(i)
