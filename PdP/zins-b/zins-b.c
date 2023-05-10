6d7dc95585e79db5d4223acb548f674a10a63f11
Wiener234 <wiemernils@gmail.com>
2023-05-10 17:40:38 +0200

    zins ready untested

--- PdP/zins-b/zins-b.c
+++ PdP/zins-b/zins-b.c
@@ -1,29 +1,65 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
 
 #define ZINS 5.0
 
 double berechne(double grundkapital, int jahr){
-    double erg = grundkapital * pow(1+(ZINS/100.0),jahr); 
-    return erg;
+    if(grundkapital >= 0 && jahr >=1){
+        double erg = grundkapital * pow(1+(ZINS/100.0),jahr); 
+        return erg;
+    }else{
+        printf("Fasche werte um in Funktion berechne ein Ergebnis zu erhalten. Überprüfen Sie ihre Eingabe.");
+        exit(1);
+    }
+}
+
+void erzeuge_kapital_list(double grundkapital, int laufzeit, double kapital[laufzeit+1]){
+    if(grundkapital >= 0 && laufzeit >=1){
+        kapital[0] = grundkapital;
+        //printf("%lf\t", kapital[0]);
+        for(int i = 1; i <=laufzeit; i++){
+            kapital[i] = berechne(grundkapital, i);
+            //printf("%lf\t", kapital[i]);
+        }
+    }else{
+        printf("Fasche werte um in Funktion erzeuge_kapital_list ein Ergebnis zu erhalten. Überprüfen Sie ihre Eingabe.");
+        exit(1);
+    }
+}
+
+void pretty_print(double grundkapital, int laufzeit, double kapital[laufzeit+1]){
+    printf("Jahr\tKapital\n");
+    //printf("%d", laufzeit);
+    for(int i = 0; i<=laufzeit; i++){
+    //    printf("%d", i);
+        printf("%4d\t%.2lf\n", i, kapital[i]);
+        //printf("%4d\t%lf", i, kapital[i]);
+    }
+
 }
 
 int main(){
 
-    double* grundkapital;
-    int* jahr;
+    double grundkapital;
+    int jahr;
 
     printf("Geben Sie ihr Grundkapital ein: ");
-    scanf("%lf\n", grundkapital);
+    scanf("%lf", &grundkapital);
 
     printf("Geben Sie die Laufzeit ein: ");
-    scanf("%d", jahr);
+    scanf("%d", &jahr);
 
-    if(*grundkapital >= 0 && *jahr >=1){
-        printf("%f", berechne(*grundkapital, *jahr));
-    }else{
-        printf("Fasche werte um in Funktion berechne ein Ergebnis zu erhalten. Überprüfen Sie ihre Eingabe.");
-        return 1;
-    }
+
+    double kapital[jahr+1];
+
+
+
+
+    printf("Erg: %f\n", berechne(grundkapital, jahr));
+    erzeuge_kapital_list(grundkapital, jahr, kapital); 
+    pretty_print(grundkapital, jahr, kapital);
+
+
     return 0;
 }
