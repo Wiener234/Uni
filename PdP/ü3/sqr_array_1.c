cc864331dd154668570e2c486effdfe876b28363
Wiener234 <wiemernils@gmail.com>
2023-06-06 12:29:10 +0200

    shit

--- PdP/ü3/sqr_array_1.c
+++ PdP/ü3/sqr_array_1.c
@@ -3,10 +3,12 @@
 
 #define zv printf("\n")
 
+#define zehn 10
+
 int main(){
-    int array[10];
-    for(int i=1; i <11;i++){
-        array[i]=pow(i, 2);
+    int array[zehn];
+    for(int i=0; i <10;i++){
+        array[i]=pow(i+1, 2);
         printf("%d\t", array[i]);
     }
     zv;
